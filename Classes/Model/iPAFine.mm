bfea2124904dc00062bb34fe127c83f0167e8306
Yonsm <Yonsm@msn.com>
2015-04-08 12:16:26 +0800

    Revert log info

--- Classes/Model/iPAFine.mm
+++ Classes/Model/iPAFine.mm
@@ -13,15 +13,15 @@
 	task.launchPath = path;
 	task.arguments = arguments;
 	if (currentDirectory) task.currentDirectoryPath = currentDirectory;
-	
+
 	NSPipe *pipe = [NSPipe pipe];
 	task.standardOutput = pipe;
 	task.standardError = pipe;
-	
+
 	NSFileHandle *file = [pipe fileHandleForReading];
-	
+
 	[task launch];
-	
+
 	NSData *data = [file readDataToEndOfFile];
 	NSString *result = data.length ? [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] : nil;
 
@@ -60,14 +60,14 @@
 
 - (uint32_t)bigEndianToSmallEndian:(uint32_t)bigEndian
 {
-    uint32_t smallEndian = 0;
-    unsigned char *small = (unsigned char *)&smallEndian;
-    unsigned char *big = (unsigned char *)&bigEndian;
-    for (int i=0; i<4; i++)
-    {
-        small[i] = big[3-i];
-    }
-    return smallEndian;
+	uint32_t smallEndian = 0;
+	unsigned char *small = (unsigned char *)&smallEndian;
+	unsigned char *big = (unsigned char *)&bigEndian;
+	for (int i=0; i<4; i++)
+	{
+		small[i] = big[3-i];
+	}
+	return smallEndian;
 }
 
 //
@@ -85,7 +85,7 @@
 	NSString *exePath = [appPath stringByAppendingPathComponent:exeName];
 
 	NSString *result = [self doTask:@"/usr/bin/lipo" arguments:[NSArray arrayWithObjects:@"-info", exePath, nil]];
-	
+
 	if (([result rangeOfString:@"armv6 armv7"].location == NSNotFound) && ([result rangeOfString:@"armv7 armv6"].location == NSNotFound))
 	{
 		return;
@@ -123,17 +123,17 @@
 	}
 
 	if ([DISPNAME hasSuffix:@"HD"]) DISPNAME = [DISPNAME substringToIndex:DISPNAME.length - 2];
-	
+
 	//
 	NSString *infoPath = [appPath stringByAppendingPathComponent:@"Info.plist"];
 	NSMutableDictionary *info = [NSMutableDictionary dictionaryWithContentsOfFile:infoPath];
-	
+
 	// 获取程序类型
 	NSArray *devices = [info objectForKey:@"UIDeviceFamily"];
 	NSUInteger family = 0;
 	for (id device in devices) family += [device intValue];
 	NSString *PREFIX = (family == 3) ? @"iOS" : ((family == 2) ? @"iPad" : @"iPhone");
-	
+
 	// 修改显示名称
 	[info setObject:DISPNAME forKey:@"CFBundleDisplayName"];
 	[info writeToFile:infoPath atomically:YES];
@@ -149,7 +149,7 @@
 			[localize writeToFile:localizePath atomically:YES];
 		}
 	}
-	
+
 	// 修改 iTunes 项目名称
 	NSString *metaPath = [[[appPath stringByDeletingLastPathComponent] stringByDeletingLastPathComponent] stringByAppendingPathComponent:@"iTunesMetadata.plist"];
 	NSMutableDictionary *meta = [NSMutableDictionary dictionaryWithContentsOfFile:metaPath];
@@ -159,11 +159,11 @@
 		[meta setObject:DISPNAME forKey:@"itemName"];
 		[meta writeToFile:metaPath atomically:YES];
 	}
-	
+
 	//
 	/*NSString *VERSION = meta ? [meta objectForKey:@"bundleShortVersionString"] : nil;
-	if (VERSION.length == 0) VERSION = [info objectForKey:@"CFBundleVersion"];
-	if (VERSION.length == 0) VERSION = [info objectForKey:@"CFBundleShortVersionString"];*/
+	 if (VERSION.length == 0) VERSION = [info objectForKey:@"CFBundleVersion"];
+	 if (VERSION.length == 0) VERSION = [info objectForKey:@"CFBundleShortVersionString"];*/
 
 	return [NSString stringWithFormat:@"%@/%@.%@.ipa", ipaPath.stringByDeletingLastPathComponent, PREFIX, DISPNAME/*, VERSION*/];
 }
@@ -180,17 +180,17 @@
 		{
 			NSInteger fromPosition = [[embeddedProvisioningLines objectAtIndex:i+1] rangeOfString:@"<string>"].location + 8;
 			NSInteger toPosition = [[embeddedProvisioningLines objectAtIndex:i+1] rangeOfString:@"</string>"].location;
-			
+
 			NSRange range;
 			range.location = fromPosition;
 			range.length = toPosition - fromPosition;
-			
+
 			NSString *identifier = [[embeddedProvisioningLines objectAtIndex:i+1] substringWithRange:range];
 			if (![identifier hasSuffix:@".*"])
 			{
 				NSRange range = [identifier rangeOfString:@"."];
 				if (range.location != NSNotFound) identifier = [identifier substringFromIndex:range.location + 1];
-				
+
 				NSDictionary *info = [NSDictionary dictionaryWithContentsOfFile:[appPath stringByAppendingPathComponent:@"Info.plist"]];
 				if (![[info objectForKey:@"CFBundleIdentifier"] isEqualToString:identifier])
 				{
@@ -205,88 +205,88 @@
 }
 
 //
-- (void)injectArchitecture:(int)fd dylibPath:(NSString *)dylibPath
+- (void)injectArchitecture:(int)fd dylibPath:(NSString *)dylibPath exePath:(NSString *)exePathForInfoOnly
 {
-    off_t archPoint = lseek(fd, 0, SEEK_CUR);
-    struct mach_header header;
-    read(fd, &header, sizeof(header));
-    if (header.magic != MH_MAGIC && header.magic != MH_MAGIC_64)
-    {
-//        _error = [NSString stringWithFormat:@"Inject failed: Invalid executable %@", exePath];
-    }
-    else
-    {
-        if (header.magic == MH_MAGIC_64)
-        {
-            int delta = sizeof(mach_header_64) - sizeof(mach_header);
-            lseek(fd, delta, SEEK_CUR);
-        }
-        
-        char *buffer = (char *)malloc(header.sizeofcmds + 2048);
-        read(fd, buffer, header.sizeofcmds);
-        
-        if ([[NSFileManager defaultManager] fileExistsAtPath:dylibPath])
-        {
-            dylibPath = [@"@executable_path" stringByAppendingPathComponent:[dylibPath lastPathComponent]];
-        }
-        const char *dylib = dylibPath.UTF8String;
-        struct dylib_command *p = (struct dylib_command *)buffer;
-        struct dylib_command *last = NULL;
-        for (uint32_t i = 0; i < header.ncmds; i++, p = (struct dylib_command *)((char *)p + p->cmdsize))
-        {
-            if (p->cmd == LC_LOAD_DYLIB || p->cmd == LC_LOAD_WEAK_DYLIB)
-            {
-                char *name = (char *)p + p->dylib.name.offset;
-                if (strcmp(dylib, name) == 0)
-                {
-//                    NSLog(@"Already Injected: %@ with %s", exePath, dylib);
-                    close(fd);
-                    return;
-                }
-                last = p;
-            }
-        }
-        
-        if ((char *)p - buffer != header.sizeofcmds)
-        {
-//            NSLog(@"LC payload not mismatch: %@", exePath);
-        }
-        
-        if (last)
-        {
-            struct dylib_command *inject = (struct dylib_command *)((char *)last + last->cmdsize);
-            char *movefrom = (char *)inject;
-            uint32_t cmdsize = sizeof(*inject) + (uint32_t)strlen(dylib) + 1;
-            cmdsize = (cmdsize + 0x10) & 0xFFFFFFF0;
-            char *moveout = (char *)inject + cmdsize;
-            for (int i = (int)(header.sizeofcmds - (movefrom - buffer) - 1); i >= 0; i--)
-            {
-                moveout[i] = movefrom[i];
-            }
-            memset(inject, 0, cmdsize);
-            inject->cmd = LC_LOAD_DYLIB;
-            inject->cmdsize = cmdsize;
-            inject->dylib.name.offset = sizeof(dylib_command);
-            inject->dylib.timestamp = 2;
-            inject->dylib.current_version = 0x00010000;
-            inject->dylib.compatibility_version = 0x00010000;
-            strcpy((char *)inject + inject->dylib.name.offset, dylib);
-            
-            header.ncmds++;
-            header.sizeofcmds += inject->cmdsize;
-            lseek(fd, archPoint, SEEK_SET);
-            write(fd, &header, sizeof(header));
-            
-            lseek(fd, archPoint + ((header.magic == MH_MAGIC_64) ? sizeof(mach_header_64) : sizeof(mach_header)), SEEK_SET);
-            write(fd, buffer, header.sizeofcmds);
-        }
-        else
-        {
-//            _error = [NSString stringWithFormat:@"Inject failed: No valid LC_LOAD_DYLIB %@", exePath];
-        }
-        
-        free(buffer);
-    }
+	off_t archPoint = lseek(fd, 0, SEEK_CUR);
+	struct mach_header header;
+	read(fd, &header, sizeof(header));
+	if (header.magic != MH_MAGIC && header.magic != MH_MAGIC_64)
+	{
+		_error = [NSString stringWithFormat:@"Inject failed: Invalid executable %@", exePathForInfoOnly];
+	}
+	else
+	{
+		if (header.magic == MH_MAGIC_64)
+		{
+			int delta = sizeof(mach_header_64) - sizeof(mach_header);
+			lseek(fd, delta, SEEK_CUR);
+		}
+
+		char *buffer = (char *)malloc(header.sizeofcmds + 2048);
+		read(fd, buffer, header.sizeofcmds);
+
+		if ([[NSFileManager defaultManager] fileExistsAtPath:dylibPath])
+		{
+			dylibPath = [@"@executable_path" stringByAppendingPathComponent:[dylibPath lastPathComponent]];
+		}
+		const char *dylib = dylibPath.UTF8String;
+		struct dylib_command *p = (struct dylib_command *)buffer;
+		struct dylib_command *last = NULL;
+		for (uint32_t i = 0; i < header.ncmds; i++, p = (struct dylib_command *)((char *)p + p->cmdsize))
+		{
+			if (p->cmd == LC_LOAD_DYLIB || p->cmd == LC_LOAD_WEAK_DYLIB)
+			{
+				char *name = (char *)p + p->dylib.name.offset;
+				if (strcmp(dylib, name) == 0)
+				{
+					NSLog(@"Already Injected: %@ with %s", exePathForInfoOnly, dylib);
+					close(fd);
+					return;
+				}
+				last = p;
+			}
+		}
+
+		if ((char *)p - buffer != header.sizeofcmds)
+		{
+			NSLog(@"LC payload not mismatch: %@", exePathForInfoOnly);
+		}
+
+		if (last)
+		{
+			struct dylib_command *inject = (struct dylib_command *)((char *)last + last->cmdsize);
+			char *movefrom = (char *)inject;
+			uint32_t cmdsize = sizeof(*inject) + (uint32_t)strlen(dylib) + 1;
+			cmdsize = (cmdsize + 0x10) & 0xFFFFFFF0;
+			char *moveout = (char *)inject + cmdsize;
+			for (int i = (int)(header.sizeofcmds - (movefrom - buffer) - 1); i >= 0; i--)
+			{
+				moveout[i] = movefrom[i];
+			}
+			memset(inject, 0, cmdsize);
+			inject->cmd = LC_LOAD_DYLIB;
+			inject->cmdsize = cmdsize;
+			inject->dylib.name.offset = sizeof(dylib_command);
+			inject->dylib.timestamp = 2;
+			inject->dylib.current_version = 0x00010000;
+			inject->dylib.compatibility_version = 0x00010000;
+			strcpy((char *)inject + inject->dylib.name.offset, dylib);
+
+			header.ncmds++;
+			header.sizeofcmds += inject->cmdsize;
+			lseek(fd, archPoint, SEEK_SET);
+			write(fd, &header, sizeof(header));
+
+			lseek(fd, archPoint + ((header.magic == MH_MAGIC_64) ? sizeof(mach_header_64) : sizeof(mach_header)), SEEK_SET);
+			write(fd, buffer, header.sizeofcmds);
+		}
+		else
+		{
+			_error = [NSString stringWithFormat:@"Inject failed: No valid LC_LOAD_DYLIB %@", exePathForInfoOnly];
+		}
+
+		free(buffer);
+	}
 }
 
 //
@@ -300,38 +300,41 @@
 	}
 	else
 	{
-        uint32_t magic;
-        read(fd, &magic, sizeof(magic));
-        if (magic == MH_MAGIC || magic == MH_MAGIC_64)
-        {
-            lseek(fd, 0, SEEK_SET);
-            [self injectArchitecture:fd dylibPath:dylibPath];
-        }
-        else if (magic == FAT_MAGIC || magic == FAT_CIGAM)
-        {
-            struct fat_header header;
-            lseek(fd, 0, SEEK_SET);
-            read(fd, &header, sizeof(fat_header));
-            int nArch = header.nfat_arch;
-            if (magic == FAT_CIGAM) nArch = [self bigEndianToSmallEndian:header.nfat_arch];
-            
-            struct fat_arch arch;
-            NSMutableArray *offsetArray = [NSMutableArray array];
-            for (int i = 0; i < nArch; i++)
-            {
-                memset(&arch, 0, sizeof(fat_arch));
-                read(fd, &arch, sizeof(fat_arch));
-                int offset = arch.offset;
-                if (magic == FAT_CIGAM) offset = [self bigEndianToSmallEndian:arch.offset];
-                [offsetArray addObject:[NSNumber numberWithUnsignedInt:offset]];
-            }
-            
-            for (NSNumber *offsetNum in offsetArray) {
-                lseek(fd, [offsetNum unsignedIntValue], SEEK_SET);
-                [self injectArchitecture:fd dylibPath:dylibPath];
-            }
-        }
-        
+		uint32_t magic;
+		read(fd, &magic, sizeof(magic));
+		if (magic == MH_MAGIC || magic == MH_MAGIC_64)
+		{
+			lseek(fd, 0, SEEK_SET);
+			[self injectArchitecture:fd dylibPath:dylibPath exePath:exePath];
+		}
+		else if (magic == FAT_MAGIC || magic == FAT_CIGAM)
+		{
+			struct fat_header header;
+			lseek(fd, 0, SEEK_SET);
+			read(fd, &header, sizeof(fat_header));
+			int nArch = header.nfat_arch;
+			if (magic == FAT_CIGAM) nArch = [self bigEndianToSmallEndian:header.nfat_arch];
+
+			struct fat_arch arch;
+			NSMutableArray *offsetArray = [NSMutableArray array];
+			for (int i = 0; i < nArch; i++)
+			{
+				memset(&arch, 0, sizeof(fat_arch));
+				read(fd, &arch, sizeof(fat_arch));
+				int offset = arch.offset;
+				if (magic == FAT_CIGAM) offset = [self bigEndianToSmallEndian:arch.offset];
+				[offsetArray addObject:[NSNumber numberWithUnsignedInt:offset]];
+			}
+
+			for (NSNumber *offsetNum in offsetArray)
+			{
+				lseek(fd, [offsetNum unsignedIntValue], SEEK_SET);
+				[self injectArchitecture:fd dylibPath:dylibPath exePath:exePath];
+				//				if (_error)
+				//					break;
+			}
+		}
+
 		close(fd);
 	}
 }
@@ -468,16 +471,16 @@
 	[[NSFileManager defaultManager] removeItemAtPath:workPath error:nil];
 }
 
-// 
+//
 - (void)refineIPA:(NSString *)ipaPath dylibPath:(NSString *)dylibPath certName:(NSString *)certName provPath:(NSString *)provPath
 {
-	// 
+	//
 	NSString *workPath = ipaPath.stringByDeletingPathExtension;//[NSTemporaryDirectory() stringByAppendingPathComponent:@"CeleWare.iPAFine"];
-	
+
 	//NSLog(@"Setting up working directory in %@",workPath);
 	[[NSFileManager defaultManager] removeItemAtPath:workPath error:nil];
 	[[NSFileManager defaultManager] createDirectoryAtPath:workPath withIntermediateDirectories:TRUE attributes:nil error:nil];
-	
+
 	// Unzip
 	_error = nil;
 	NSString *appPath = [self unzipIPA:ipaPath workPath:workPath];
